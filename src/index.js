481676045f50761f83283902ead3b057a3ed16b1
Yelyzaveta Oksiuk <yelyzaveta.oksiuk@gmail.com>
2020-04-30 00:45:39 +0300

    src/index.js, test/index.js added

---
+++ src/index.js
@@ -0,0 +1,85 @@
+/* ДЗ 2 - работа с массивами и объектами */
+
+/*
+ Задание 1:
+
+ Напишите аналог встроенного метода forEach для работы с массивами
+ Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
+ */
+function forEach(array, fn) {
+  for(let i = 0; i < array.length; ++i) {
+    fn(array[i]);  //fn(array[i], i, array)
+  }
+}
+
+/*
+ Задание 2:
+
+ Напишите аналог встроенного метода map для работы с массивами
+ Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
+ */
+function map(array, fn) {
+  let newArray = [];
+  for(let i = 0; i < array.length; ++i) {
+    let item = fn(array[i]);
+    newArray.push(item);
+  }
+  return newArray;
+}
+
+/*
+ Задание 3:
+
+ Напишите аналог встроенного метода reduce для работы с массивами
+ Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
+ */
+function reduce(array, fn, initial) {
+  for(let i = 0; i < array.length; ++i) {
+    initial += fn(array[i]);
+  }
+  return initial;
+}
+// let ar = [1, 2, 3, 4];
+// console.log(ar.reduce((a,b) =>a - b));
+/*
+ Задание 4:
+
+ Функция должна перебрать все свойства объекта, преобразовать их имена в верхний регистр и вернуть в виде массива
+
+ Пример:
+   upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
+ */
+function upperProps(obj) {
+}
+
+/*
+ Задание 5 *:
+
+ Напишите аналог встроенного метода slice для работы с массивами
+ Посмотрите как работает slice и повторите это поведение для массива, который будет передан в параметре array
+ */
+function slice(array, from, to) {
+  let newArray;
+  for(let i = from; i < to; ++i) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
+
+/*
+ Задание 6 *:
+
+ Функция принимает объект и должна вернуть Proxy для этого объекта
+ Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
+ */
+function createProxy(obj) {
+}
+
+export {
+    forEach,
+    map,
+    reduce,
+    upperProps,
+    slice,
+    createProxy
+};
