fd5235129d5b8d7f01cbc2c6c8929a97b41b4877
Yelyzaveta Oksiuk <yelyzaveta.oksiuk@gmail.com>
2020-05-01 19:30:54 +0300

    4 tasks done

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@
  Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
  */
 function forEach(array, fn) {
-  for(let i = 0; i < array.length; ++i) {
-    fn(array[i]);  //fn(array[i], i, array)
-  }
+    for (let i = 0; i < array.length; ++i) {
+        fn(array[i], i, array);
+    }
 }
 
 /*
@@ -19,12 +19,15 @@ function forEach(array, fn) {
  Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
  */
 function map(array, fn) {
-  let newArray = [];
-  for(let i = 0; i < array.length; ++i) {
-    let item = fn(array[i]);
-    newArray.push(item);
-  }
-  return newArray;
+    let newArray = [];
+
+    for (let i = 0; i < array.length; ++i) {
+        let item = fn(array[i], i, array);
+
+        newArray.push(item);
+    }
+
+    return newArray;
 }
 
 /*
@@ -34,13 +37,24 @@ function map(array, fn) {
  Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
  */
 function reduce(array, fn, initial) {
-  for(let i = 0; i < array.length; ++i) {
-    initial += fn(array[i]);
-  }
-  return initial;
+    let accumulator;
+    let index;
+
+    if (initial) {
+        accumulator = initial;
+        index = 0;
+    } else {
+        accumulator = array[0];
+        index = 1;
+    }
+
+    for (let i = index; i < array.length; ++i) {
+        accumulator = fn(accumulator, array[i], i, array);
+    }
+
+    return accumulator;
 }
-// let ar = [1, 2, 3, 4];
-// console.log(ar.reduce((a,b) =>a - b));
+
 /*
  Задание 4:
 
@@ -50,6 +64,7 @@ function reduce(array, fn, initial) {
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
 function upperProps(obj) {
+    return Object.keys(obj).map(key => key.toUpperCase());
 }
 
 /*
@@ -59,11 +74,49 @@ function upperProps(obj) {
  Посмотрите как работает slice и повторите это поведение для массива, который будет передан в параметре array
  */
 function slice(array, from, to) {
-  let newArray;
-  for(let i = from; i < to; ++i) {
-    newArray.push(array[i]);
-  }
-  return newArray;
+    let newArray = [];
+    let length = array.length;
+    let start, end;
+
+    switch (true) {
+        case from === undefined:
+            start = 0;
+            break;
+        case from >= length:
+            return newArray;
+        case from >= 0 && from < length: 
+            start = from;
+            break;
+        case from < 0 && -from < length:
+            start = length + from;
+            break;
+        case from < 0 && -from >= length: 
+            start = 0;
+            break;
+    }
+
+    switch (true) {
+        case to === undefined:
+            end = length;
+            break;
+        case to > 0 && to <= length:
+            end = to;
+            break;
+        case to > length:
+            end = length;
+            break;
+        case to < 0 && -to <= length:
+            end = length + to;
+            break;
+        case to < 0 && -to > length:
+            return newArray;
+    }
+
+    for (let i = start; i < end; ++i) {
+        newArray.push(array[i]);
+    }
+
+    return newArray;
 }
 
 /*
